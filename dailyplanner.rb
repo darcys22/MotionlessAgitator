29c7d14d042a324148fb8601924d75695f264428
Sean Darcy <darcys22@gmail.com>
2014-02-07 11:31:23 -0800

    haha back to 4.0

--- dailyplanner.rb
+++ dailyplanner.rb
@@ -8,7 +8,7 @@ module MotionlessAgitator
         end
 
         def plan!
-            shifts_by_options_count = count_shift_pool
+            shifts_by_options_count = count_occurance(:Shift)
             shifts_by_options_count = shifts_by_options_count.sort_by { |shift, count| count}
             shifts_by_options_count.each do |shift, count|
                 employee = find_least_available_employee(shift)
@@ -18,7 +18,7 @@ module MotionlessAgitator
         end
 
         def find_least_available_employee(shift)
-            shift_count = count_employee_occurance
+            shift_count = count_occurance(:Employee)
             shift_count.sort_by {|employee, count| count}
             shift_count.each do |employee, count|
                 return employee if @daily_options.include?([shift,employee])
@@ -26,17 +26,6 @@ module MotionlessAgitator
             nil
         end
 
-        def count_shift_pool
-            @daily_options.inject({}) do |count, option|
-                if count.include?(option[0])
-                    count[option[0]] += 1
-                else
-                    count[option[0]] = 1
-                end
-                count
-            end
-        end
-        
         def add_shift(shift, employee)
             unless @daily_schedule.include?(shift)
                 @daily_schedule[shift] = employee
@@ -45,12 +34,18 @@ module MotionlessAgitator
             end
         end
 
-        def count_employee_occurance
+        def count_occurance(type = nil)
+            if type == :Shift 
+                position = 0
+            elsif type == :Employee
+                position = 1
+            end
+
             @daily_options.inject({}) do |count, option|
-                if count.include?(option[1])
-                    count[option[1]] += 1
+                if count.include?(option[position])
+                    count[option[position]] += 1
                 else
-                    count[option[1]] = 1
+                    count[option[position]] = 1
                 end
                 count
             end
